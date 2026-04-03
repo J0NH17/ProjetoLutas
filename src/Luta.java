ca1d1fbff8687995ad495ee1360397c9d230e08d
J0NH17 <1091771@senacminas.edu.br>
2026-04-02 21:43:27 -0300

    TESTE 2 comandos git

--- src/Luta.java
+++ src/Luta.java
@@ -1,39 +1,87 @@
 
+import java.util.Random;
 
 public class Luta {
-    
-    
+
+    Random r = new Random();
     private Lutador desafiante;
     private Lutador desafiado;
-    private int rounds;
+    private int rounds = 0;
     private boolean aprovado;
-    
-    public void marcarLuta(Lutador desafiante,Lutador desafiado){
-        if(desafiante.getCategoria()!= desafiado.getCategoria()){
+
+    public void marcarLuta(Lutador desafiante, Lutador desafiado) {
+        if (desafiante.getCategoria() != desafiado.getCategoria()) {
             System.out.println("Essa luta não pode ocorrer por lutadores serem de categorias diferentes!");
             setAprovado(false);
-        
-        }else{
+
+        } else {
             System.out.println("Luta marcada!!");
             setAprovado(true);
-        
+
         }
-        
-       
+
     }
-    public void luta (){
+
+    public void luta(Lutador desafiante, Lutador desafiado) {
         
-        if(isAprovado()==true){
+        //se lutar ↴
+        Lutador vencedor ;
+
+        if (isAprovado() == true) {
+            do {
+                int resultado = r.nextInt(3);
+
+                if (resultado == 0) {
+
+                    //Vitoria do desafiante
+                    desafiante.ganharLuta();
+                    desafiado.perdeLuta();
+                    vencedor = desafiante;
+                    System.out.println(desafiante.getNome() + " É o vencedor do "+(rounds +1) +"° round ");
+
+                } else if (resultado == 1) {
+
+                    //Vitoria do desafiado 
+                    desafiado.ganharLuta();
+                    desafiante.perdeLuta();
+                    vencedor = desafiado;
+                    System.out.println(desafiado.getNome() + " É o vencedor do "+(rounds +1)+"° round ");
+
+                } else {
+
+                    //EMPATE
+                    desafiante.empatarLuta();
+                    desafiado.empatarLuta();
+                    System.out.println((rounds +1)+"° Round Empatado!! ");
+                }
+                rounds++;
+            } while (rounds != 7 || (desafiante.getVitorias() == 3 || desafiado.getVitorias() == 3));
             
-            System.out.println("lutando");
-        
-        }else{
+            if(desafiante.getVitorias()>=3){
+                vencedor = desafiante;
+                System.out.println("VENCEDOR : "+vencedor.getNome());
             
+            }else if(desafiado.getVitorias()>=3){
+                vencedor = desafiado;
+                System.out.println("VENCEDOR : "+vencedor.getNome());
+            
+            }else{
+                
+                System.out.println("Empatado!! ");
+            
+            }
+            
+  
+            //Nao entendi como fazer rouds (* ￣︿￣)     tentei um loop ...
+
+           
+
+        } else {
+
             System.out.println("AHHH SEM LUTA DEU ERRO ALGO AE !");
-            
+
         }
-     
-    
+
     }
 
     public Lutador getDesafiante() {
@@ -67,8 +115,5 @@ public class Luta {
     public void setAprovado(boolean aprovado) {
         this.aprovado = aprovado;
     }
-    
-    
-   
-}
 
+}
